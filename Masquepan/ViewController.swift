4a5f8d569e1d476cb577ef6bc09b356413f48eea
dam <dam@imac-de-dam-2.local>
2018-02-16 14:37:21 +0100

    First working Login

--- Masquepan/ViewController.swift
+++ Masquepan/ViewController.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, OnHttpResponse {
+    
+    @IBOutlet weak var tfUser: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    var response: (Any)? = nil
+    
+    func onDataReceived(data: Data) {
+        if let resp = try? JSONSerialization.jsonObject(with: data, options: []){
+            print(String(describing: resp))
+            response = resp
+        }
+    }
+    
+    func onErrorReceivingData(message: String) {}
+    
+    func toBase64(cadena: String) -> String? {
+        guard let data = cadena.data(using: .utf8) else {
+            return nil
+        }
+        return Data(data).base64EncodedString()
+    }
 
+    @IBAction func btLogin(_ sender: UIButton) {
+        guard tfUser.text != "" || tfPassword.text != "" else {
+            return
+        }
+        let userPass = tfUser.text! + ":" + tfPassword.text!
+        guard let cliente = ClienteHttp(target: "", authorization: "Basic " + toBase64(cadena: userPass)!, responseObject: self) else {
+            return
+        }
+        cliente.request()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
